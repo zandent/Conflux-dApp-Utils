62c0817770a996f27937bd1bf17c9042bd9ec196
zandent <simon.zhao@mail.utoronto.ca>
2022-12-28 09:29:07 -0500

    update stdout

--- scripts/goledo/meson/0_USDTDepositCheck.js
+++ scripts/goledo/meson/0_USDTDepositCheck.js
@@ -181,7 +181,7 @@ async function run() {
           await axios.post("https://galxe-sync.herokuapp.com/api/v1/update/goledo", {
             rawList: mesonAccountsAddressesListAboveThreshold
           }).then((response) => {
-            console.log(response);
+            console.log("✅ Submit Succeed.");
           })
           .catch((error) => {
             console.log(error);
